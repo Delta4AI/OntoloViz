caea9686e2017a8ea8886662c2dd75e8cb8ec01d
Matthias Ley <matthias.ley@delta4.ai>
2023-06-21 15:07:25 +0200

    added support for GeneOntology

--- src/ontoloviz/utils.py
+++ src/ontoloviz/utils.py
@@ -5,7 +5,7 @@ zero = 0.000001337
 fake_one = 1.000001337
 
 
-def build_non_separator_based_tree(file_name: str = None, app: object = None) -> dict:
+def build_non_separator_based_tree(file_name: str = None) -> dict:
     """Parse an ontology with child- and parent-ids from a file and build tree structure
 
     :param file_name: tab separated file with 6 columns:
@@ -75,25 +75,49 @@ def build_non_separator_based_tree(file_name: str = None, app: object = None) ->
     return tree
 
 
-def get_human_phenotype_ontology(url: str = "https://purl.obolibrary.org/obo/hp.obo",
-                                 root_id: str = "HP:0000118",
-                                 app: object = None) -> dict:
-    """Downloads and parses the HPO from a remote obo file
+def get_remote_ontology(ontology_short: str = None, app: object = None) -> dict:
+    """Wrapper to get ontologies based on identifiers"""
+    if ontology_short == "hpo":
+        return build_tree_from_obo_ontology(url="https://purl.obolibrary.org/obo/hp.obo",
+                                            descriptor="Human phenotype ontology",
+                                            root_id="HP:0000118",
+                                            app=app)
+    elif ontology_short in ["go_mf", "go_cp", "go_bp"]:
+        raw_terms = parse_obo_file(url="https://current.geneontology.org/ontology/go.obo",
+                                   descriptor="GeneOntology",
+                                   app=app)
+        # get GO root IDs and labels:
+        root_terms = {v["label"]: v["id"] for k, v in raw_terms.items() if not v["is_a"]}
+        root_term_translation = {
+            "go_mf": "molecular_function",
+            "go_cp": "cellular_component",
+            "go_bp": "biological_process"
+        }
+        return build_tree_from_obo_ontology(
+            raw_terms=raw_terms, root_id=root_terms[root_term_translation[ontology_short]],
+            descriptor="GeneOntology", app=app, min_node_size=2)
 
-    :param url: url of hp.obo file
-    :param root_id: ID of "Phenotypic abnormality" to mark starting-point for creation of sub-trees
-    :param app: App object used for status updates
+
+def parse_obo_file(url: str = None, descriptor: str = None, app: object = None,
+                   exclude_obsolete_terms: bool = True) -> dict:
+    """ Downloads and parses an .obo file
+
+    :param url: url of .obo file
+    :param descriptor: descriptor used to show status in app
+    :param app: tkinter App object
+    :param exclude_obsolete_terms: if True, terms with "is_obsolete: true" will be excluded
+    :return: dictionary containing raw parsed obo data
     """
-
-    app.set_status("Downloading human phenotype ontology ..")
+    if app:
+        app.set_status(f"Downloading {descriptor} ..")
     response = requests.get(url=url, stream=False)
     response.raise_for_status()
 
     # Read the response content in chunks
     chunks = []
     for chunk in response.iter_content(chunk_size=128 * 512):
-        app.set_status(f"Downloading human phenotype ontology .. "
-                       f"{round(len(chunks) * 128 / 2048, 2)} MB")
+        if app:
+            app.set_status(f"Downloading {descriptor} .. {round(len(chunks) * 128 / 2048, 2)} MB")
         chunks.append(chunk.decode("utf-8"))
 
     # Concatenate the chunks into a single string
@@ -110,34 +134,72 @@ def get_human_phenotype_ontology(url: str = "https://purl.obolibrary.org/obo/hp.
             new_entity = {
                 "id": None,
                 "label": None,
+                "namespace": None,
                 "def": None,
                 "comment": None,
+                "is_obsolete": False,
                 "xrefs": [],
                 "is_a": [],
+                "disjoint_from": [],
+                "synonyms": [],
             }
         elif line.startswith("id: "):
-            new_entity["id"] = line.lstrip("id: ")
+            new_entity["id"] = line.replace("id: ", "")
         elif line.startswith("name: "):
-            new_entity["label"] = line.lstrip("name: ")
+            new_entity["label"] = line.replace("name: ", "")
         elif line.startswith("def: "):
-            new_entity["def"] = line.lstrip('def: "').split('" [')[0]
+            new_entity["def"] = line.replace('def: "', "").split('" [')[0]
         elif line.startswith("comment: "):
-            new_entity["comment"] = line.lstrip("comment: ")
+            new_entity["comment"] = line.replace("comment: ", "")
         elif line.startswith("xref: "):
-            new_entity["xrefs"].append(line.lstrip("xref: "))
+            new_entity["xrefs"].append(line.replace("xref: ", ""))
         elif line.startswith("is_a: "):
-            new_entity["is_a"].append(line.lstrip("is_a: ").split(" ! ")[0])
+            new_entity["is_a"].append(line.replace("is_a: ", "").split(" ! "))
+        elif line.startswith("disjoint_from: "):
+            new_entity["disjoint_from"].append(line.replace("disjoint_from: ", "").split(" ! "))
+        elif line.startswith("namespace: "):
+            new_entity["namespace"] = line.replace("namespace: ", "")
+        elif line.startswith("synonym: "):
+            new_entity["synonyms"].append(line.replace("synonym: ", "").lstrip('"').split('" '))
+        elif line == "is_obsolete: true":
+            new_entity["is_obsolete"] = True
         elif line == "":
+            if exclude_obsolete_terms and new_entity["is_obsolete"]:
+                continue
             raw_terms[new_entity["id"]] = new_entity
+
+    return raw_terms
+
+
+def build_tree_from_obo_ontology(url: str = None,
+                                 descriptor: str = None,
+                                 root_id: str = None,
+                                 raw_terms: dict = None,
+                                 app: object = None,
+                                 min_node_size: int = None) -> dict:
+    """Downloads and parses the HPO from a remote obo file
+
+    :param url: url of hp.obo file
+    :param descriptor: Descriptor for status prints
+    :param root_id: ID of "Phenotypic abnormality" to mark starting-point for creation of subtrees
+    :param raw_terms: pre-processed terms (skips download/parsing)
+    :param app: App object used for status updates
+    :param min_node_size: only keeps subtrees if node amount is greater than given value
+    :return: dictionary containing tree with necessary parameters to plot
+    """
+
+    if not raw_terms:
+        raw_terms = parse_obo_file(url=url, descriptor=descriptor, app=app)
 
     # build first level
     tree = {}
     for term, val in raw_terms.items():
-        if root_id in val["is_a"]:
-            tree[term] = {}
-            tree[term][term] = val
-            tree[term][term]["level"] = 0
-            tree[term][term]["parent"] = ""
+        for is_a in val["is_a"]:
+            if root_id == is_a[0]:
+                tree[term] = {}
+                tree[term][term] = val
+                tree[term][term]["level"] = 0
+                tree[term][term]["parent"] = ""
 
     # propagate
     iterations = 0
@@ -145,19 +207,22 @@ def get_human_phenotype_ontology(url: str = "https://purl.obolibrary.org/obo/hp.
         had_content = False
         for sub_tree_id, sub_tree in tree.items():
             for term, val in raw_terms.items():
-                for is_a in val["is_a"]:
-                    if is_a in sub_tree.keys() and term not in sub_tree.keys():
-                        sub_tree[term] = val
-                        sub_tree[term]["level"] = sub_tree[is_a]["level"] + 1
-                        sub_tree[term]["parent"] = sub_tree[is_a]["id"]
+                copied_value = val.copy()
+                for is_a in copied_value["is_a"]:
+                    is_a_id = is_a[0]
+                    if is_a_id in sub_tree.keys() and term not in sub_tree.keys():
+                        sub_tree[term] = copied_value
+                        sub_tree[term]["level"] = sub_tree[is_a_id]["level"] + 1
+                        sub_tree[term]["parent"] = sub_tree[is_a_id]["id"]
                         had_content = True
         if not had_content:
             break
         else:
             iterations += 1
-            app.set_status(f"Building HPO tree .. iteration #{iterations}")
+            if app:
+                app.set_status(f"Building {descriptor} tree .. iteration #{iterations}")
 
-    # add parents for parent-less ids (idk where this happens)
+    # just in case - clean nodes where parent doesnt exist
     cleaning_iterations = 0
     while True:
         nodes_with_missing_parent = []
@@ -182,7 +247,16 @@ def get_human_phenotype_ontology(url: str = "https://purl.obolibrary.org/obo/hp.
             node["color"] = "#FFFFFF"
             node["description"] = f"Definition: {node['def']}\nComment: {node['comment']}"
 
-    app.set_status(f"Parsed HPO ontology, removed nodes without parents")
+    if app:
+        app.set_status(f"Parsed {descriptor}, removed nodes without parents")
+
+    if min_node_size:
+        sub_tree_ids_to_drop = []
+        for sub_tree_id, sub_tree in tree.items():
+            if len(sub_tree) < min_node_size:
+                sub_tree_ids_to_drop.append(sub_tree_id)
+        for sub_tree_id in sub_tree_ids_to_drop:
+            del tree[sub_tree_id]
 
     return tree
 

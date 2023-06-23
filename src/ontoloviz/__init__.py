30e1bb4b0d57bbf04c43fca188810638ba6b4ed1
Matthias Ley <matthias.ley@delta4.ai>
2023-06-23 13:34:48 +0200

    cleaned up imports

--- src/ontoloviz/__init__.py
+++ src/ontoloviz/__init__.py
@@ -1,3 +1,3 @@
-from .core import ATCSunburst, MeSHSunburst, rgb_to_hex, hex_to_rgb, chunks, generate_color_range
+from .core import ATCSunburst, MeSHSunburst, chunks, generate_color_range
 from .app import App, run_app
 
